b15e835b78d32912be3c960c2b73cefbddf06790
Victor <rusvictor@hotmail.es>
2023-03-15 18:23:09 +0100

    15/03/23 class pt2

--- src/main/java/com/victorsashnev/tema10/Ejer02/ArrayListEstadistica.java
+++ src/main/java/com/victorsashnev/tema10/Ejer02/ArrayListEstadistica.java
@@ -1,6 +1,21 @@
 package com.victorsashnev.tema10.Ejer02;
 
+import java.util.ArrayList;
+
 public class ArrayListEstadistica implements  IEstadisticas{
+
+    public ArrayListEstadistica(int[] aray){
+        ArrayList<Integer> list = arayList(aray);
+
+    }
+    public ArrayList<Integer> arayList (int[] aray){
+        ArrayList<Integer> arayList =  new ArrayList<Integer>();
+        for (int j : aray) {
+           arayList.add(j);
+        }
+        return arayList;
+    }
+
 
     @Override
     public double minimo() {
--- src/main/java/com/victorsashnev/tema10/Ejer02/Ejer02.java
+++ src/main/java/com/victorsashnev/tema10/Ejer02/Ejer02.java
@@ -1,7 +1,14 @@
 package com.victorsashnev.tema10.Ejer02;
 
+import com.victorsashnev.tema10.Lib;
+
+import java.util.ArrayList;
+
 public class Ejer02 {
+    private static int[]aray;
     public Ejer02(){
+        aray = new int[10];
+        aray = Lib.fillArrayRandom(aray,50,0);
 
     }
 }
