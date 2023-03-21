3e5a9ff263a055379d21357138ffb138624459af
Victor <rusvictor@hotmail.es>
2023-03-21 14:35:31 +0100

    21/03/23 home

--- src/main/java/com/victorsashnev/tema10/Ejer05/Estadistica.java
+++ src/main/java/com/victorsashnev/tema10/Ejer05/Estadistica.java
@@ -2,6 +2,7 @@ package com.victorsashnev.tema10.Ejer05;
 
 import com.victorsashnev.tema10.Lib;
 
+import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Random;
 
@@ -26,6 +27,14 @@ public class Estadistica {
             }
             double weight = Lib.randomNumeberDouble(80,20);
             double height = Lib.randomNumeberDouble(2,0);
+            pacientes[i] = new Paciente(id,calendar,sexo,weight,height);
+
+        }
+    }
+    private void mayorMenorPaciente(){
+        StringBuilder sb = new StringBuilder();
+        Calendar calendar = Calendar.getInstance();
+        for(int i = 0; i < pacientes.length;i++){
 
         }
     }
